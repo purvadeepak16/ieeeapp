76f20ff2b91918b84d4af8f3a4810d5d98ac1f21
Riyas code tonic <riyamp0606@gmail.com>
2026-01-31 19:57:00 +0530

    Add profile and edit profile screens

---
+++ lib/widgets/profile_section_card.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ProfileSectionCard extends StatelessWidget {
+  final List<Widget> children;
+
+  const ProfileSectionCard({
+    super.key,
+    required this.children,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 6),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(14),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.06),
+            blurRadius: 10,
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Column(children: children),
+    );
+  }
+}
